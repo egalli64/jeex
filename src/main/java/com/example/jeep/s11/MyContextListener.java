69293501a2b1346b580b4be9dacb22c2dca2b124
Emanuele <egalli64@gmail.com>
2022-10-03 18:09:22 +0200

    extra

---
+++ src/main/java/com/example/jeep/s11/MyContextListener.java
@@ -0,0 +1,20 @@
+package com.example.jeep.s11;
+
+import java.time.LocalTime;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@WebListener
+public class MyContextListener implements ServletContextListener {
+    private static final Logger log = LogManager.getLogger(MyContextListener.class);
+
+    public void contextInitialized(ServletContextEvent sce) {
+        log.traceEntry();
+        sce.getServletContext().setAttribute("start", LocalTime.now());
+    }
+}
