cba96b846d2660606eafdbf2dd0ceacbd72f9735
Emanuele <egalli64@gmail.com>
2024-08-15 21:00:01 +0200

    merge

---
+++ src/main/java/com/example/jeex/m2/s2/CoderAllServlet.java
@@ -0,0 +1,46 @@
+/*
+ * Going deeper into Jakarta Enterprise Edition
+ * 
+ * https://github.com/egalli64/jeex
+ */
+package com.example.jeex.m2.s2;
+
+import java.io.IOException;
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Get all coders
+ */
+@SuppressWarnings("serial")
+@WebServlet("/m2/s2/coders")
+public class CoderAllServlet extends HttpServlet {
+    private static final Logger log = LogManager.getLogger(CoderAllServlet.class);
+
+    /**
+     * JDBC data source, container managed
+     */
+    @Resource(name = "jdbc/hron")
+    private DataSource ds;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        log.traceEntry();
+
+        try (CoderDao dao = new CoderDao(ds)) {
+            request.setAttribute("coders", dao.getAll());
+        }
+
+        request.getRequestDispatcher("coders.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/m2/s2/coders.jsp
@@ -0,0 +1,41 @@
+<%-- 
+ * Going deeper into Jakarta Enterprise Edition
+ * 
+ * https://github.com/egalli64/jeex
+ --%>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Coders</title>
+<%@include file="/include/link.html"%>
+</head>
+<body>
+    <header>
+        <h1>Coders</h1>
+        <%@include file="/include/navHome.html"%>
+    </header>
+
+    <table>
+        <tr>
+            <th>id</th>
+            <th>first name</th>
+            <th>last name</th>
+            <th>hired</th>
+            <th>salary</th>
+        </tr>
+        <c:forEach var="coder" items="${coders}">
+            <tr>
+                <td>${coder.id}</td>
+                <td>${coder.firstName}</td>
+                <td>${coder.lastName}</td>
+                <td>${coder.hired}</td>
+                <td>${coder.salary}</td>
+            </tr>
+        </c:forEach>
+    </table>
+</body>
+</html>
